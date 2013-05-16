27e193b4c650786f1c96490763ec2af70f3f8edd
h0ng10 <hansmartin.muench@googlemail.com>
2013-05-16 19:18:19 +0200

    Added version.rb

---
+++ lib/msf_lint/version.rb
@@ -0,0 +1,5 @@
+module MsfLint
+  # MsfLint follows the {http://semver.org/  Semantic Versioning Specification}.  At this time, the API
+  # is considered unstable. Because of the unstable API the version should remain below 1.0.0
+  VERSION = '0.0.2'
+end
