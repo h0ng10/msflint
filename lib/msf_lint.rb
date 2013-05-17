6afef5440fdfe5e51289a1e887c183e1b634aa59
h0ng10 <hansmartin.muench@googlemail.com>
2013-05-17 08:14:58 +0200

    checks go into msf_lint directory

--- lib/msf_lint.rb
+++ lib/msf_lint.rb
@@ -2,10 +2,25 @@
 
 require "parser/ruby19"
 require "ast"
-require "msf_lint/node"
+require "msf_lint/ast/node"
 require "msf_lint/note"
-require "msf_lint/check"
+require "msf_lint/checks/check"
 require "msf_lint/code_walker/base_walker"
 require "msf_lint/code_walker/line_walker"
 require "msf_lint/code_walker/ast_node_walker"
 
+require 'msf_lint/checks/for_loop_check'
+require 'msf_lint/checks/load_usage_check'
+require 'msf_lint/checks/line_length_check'
+require 'msf_lint/checks/bad_indent_check'
+require 'msf_lint/checks/end_of_line_check'
+require 'msf_lint/checks/name_check'
+require 'msf_lint/checks/function_basics_check'
+require 'msf_lint/checks/ranking_check'
+require 'msf_lint/checks/register_option_check'
+require 'msf_lint/checks/request_cgi_check'
+require 'msf_lint/checks/file_open_binary_check'
+require 'msf_lint/checks/unused_parameter_check'
+require 'msf_lint/checks/global_variable_check'
+require 'msf_lint/checks/update_info_check'
+require 'msf_lint/checks/empty_rescue_check'
--- msflint.rb
+++ msflint.rb
@@ -6,21 +6,6 @@ $LOAD_PATH << File.dirname(__FILE__) + "/lib"
 $LOAD_PATH << File.dirname(__FILE__) 
 
 require 'msf_lint'
-require 'checks/for_loop_check'
-require 'checks/load_usage_check'
-require 'checks/line_length_check'
-require 'checks/bad_indent_check'
-require 'checks/end_of_line_check'
-require 'checks/name_check'
-require 'checks/function_basics_check'
-require 'checks/ranking_check'
-require 'checks/register_option_check'
-require 'checks/request_cgi_check'
-require 'checks/file_open_binary_check'
-require 'checks/unused_parameter_check'
-require 'checks/global_variable_check'
-require 'checks/update_info_check'
-require 'checks/empty_rescue_check'
 
 class String
 	def red
